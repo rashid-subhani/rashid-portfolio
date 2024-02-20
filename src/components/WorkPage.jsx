d204e96d123e30425125d5d64236be398e7c9b74
Rashid Subhani <ras_sub2003@yahoo.com>
2024-02-20 17:23:29 +0000

    Workpage created and added projects

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -3,24 +3,24 @@
 import Work from "./Work";
 
 const projects = [
-    // {
-    //     title: "weather-dashboard",
-    //     link: "https://github.com/rashid-subhani/weather-dashboard",
-    //     imgSrc: "/weather.jpg",
-    //     description: "A responsive weather dashboard that provides real-time weather information, featuring dynamic data fetching and a user-friendly interface."
-    // },
-    // {
-    //     title: "team-profile-generator",
-    //     link: "https://github.com/rashid-subhani/team--profile-generator",
-    //     imgSrc: "/team-sample.png",
-    //     description: "Empower's team identity with Team profile generator. A user friendly tool designed for quick and concise team introductions."
-    // },
-    // {
-    //     title: "work-day-schedular",
-    //     link: "https://github.com/rashid-subhani/work-day-schedular",
-    //     imgSrc : "/shedular.jpg",
-    //     description : "A productivity-enhancing application designed to help users organize and optimize their work day."
-    // }
+    {
+        title: "weather-dashboard",
+        link: "https://github.com/rashid-subhani/weather-dashboard",
+        imgSrc: "/weather.jpg",
+        description: "A responsive weather dashboard that provides real-time weather information, featuring dynamic data fetching and a user-friendly interface."
+    },
+    {
+        title: "team-profile-generator",
+        link: "https://github.com/rashid-subhani/team--profile-generator",
+        imgSrc: "/team-sample.png",
+        description: "Empower's team identity with Team profile generator. A user friendly tool designed for quick and concise team introductions."
+    },
+    {
+        title: "work-day-schedular",
+        link: "https://github.com/rashid-subhani/work-day-schedular",
+        imgSrc : "/shedular.jpg",
+        description : "A productivity-enhancing application designed to help users organize and optimize their work day."
+    }
 ]
 
 function WorkPage() {
