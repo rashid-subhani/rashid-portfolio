ed7724600389cf6f42d1da1fb2a60cac2e9960db
Rashid Subhani <ras_sub2003@yahoo.com>
2024-02-28 02:35:45 +0000

    description modified in about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,27 @@
 // AboutMe.js
-import React from 'react';
+import React from "react";
 
 function AboutMe() {
   return (
     <section className="page-section about-me" id="about">
       <h2 id="h2">About Me</h2>
       <p>
-      I am a web developer. I completed Master's in Public Administration as well as Banking, Finance and Management. After that, I started my journey as Web Developer. I have worked on various projects.
+        Welcome to my portfolio! I am a dedicated and skilled web developer with
+        a passion for crafting seamless and visually appealing online
+        experiences. My journey in web development began with a comprehensive
+        Front-End Web Development by edX, where I honed my skills and
+        gained a solid foundation in the latest industry practices. 
+        
+        Throughout the course, I actively engaged in hands-on projects, applying
+        theoretical knowledge to real-world scenarios. These projects, featured
+        in my portfolio, showcase my proficiency in HTML, CSS, JavaScript, and
+        other essential technologies. I am eager to contribute my expertise to
+        dynamic and innovative projects. 
+        
+        I am excited about the prospect of collaborating with forward-thinking
+        employers who value creativity and excellence in web development. Feel
+        free to explore my portfolio and reach out to discuss how I can bring my
+        skills to your team. Together, let's build the future of the web!
       </p>
     </section>
   );
